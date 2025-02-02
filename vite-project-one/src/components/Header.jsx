aabc194248503717d93007d88cce79e3af62354b
Nima Karkhaneh <nimakarkhaneh@gmail.com>
2025-02-02 23:46:33 +1100

    adds the first project

---
+++ vite-project-one/src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return(
+        <header>
+            <nav>
+                <img src="/react-logo.png" alt="React Logo"/>
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
